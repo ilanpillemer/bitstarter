1d60590051a1c2b9e0798d32b64aa5cd4cf418e4
Ilan Pillemer <Ilan.Pillemer@gmail.com>
2013-07-06 17:53:12 +0000

    someone messed with my emacs bindings

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    b =  fs.readFileSynch("index.html");
-    Response.Send(b.toString());
+    b = fs.readFileSynch("index.html");
+    response.send(b.toString());
 });
 
-Var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
