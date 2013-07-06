97da826b7e6157b0f6b9fc607ea8589be12ed5c7
Ilan Pillemer <Ilan.Pillemer@gmail.com>
2013-07-06 17:45:26 +0000

    added require fs to web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
+require('fs')
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     b =  fs.readFileSynch("index.html");
-    Response.Send(b.toString());
+    Response.Send(b.Tostring());
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
